c891a18447f39462e7b1da06946c3ec39ae74e68
Arto Bendiken <arto@conreality.org>
2018-02-15 19:49:45 +0200

    Added a guard for the SSH host key.

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -3,6 +3,9 @@
 mkdir -p /etc/dropbear
 chown root:root /etc/dropbear
 chmod 700 /etc/dropbear
-/usr/bin/dropbearkey -t ecdsa -f /etc/dropbear/dropbear_ecdsa_host_key
+
+if [ ! -f /etc/dropbear/dropbear_ecdsa_host_key ]; then
+  /usr/bin/dropbearkey -t ecdsa -f /etc/dropbear/dropbear_ecdsa_host_key
+fi
 
 exec "$@"
